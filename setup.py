7036d69e3d411a459f60eac4d76b30c12ec429a4
Demian Brecht <dbrecht@salesforce.com>
2018-10-24 14:01:38 -0700

    Change src to SalesforcePy
    The current method works when installing through pip, but breaks with
    other installation methods (namely `python setup.py (develop|install)`.
    This change fixes that.

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from setuptools import setup
+import setuptools
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setup(name='SalesforcePy',
-      version='1.0.1',
-      description='An absurdly simple package for making Salesforce Rest API requests',
-      url='https://github.com/forcedotcom/SalesforcePy',
-      author='Aaron Caffrey',
-      author_email='acaffrey@salesforce.com',
-      license='BSD',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-      ],
-      packages=['SalesforcePy'],
-      zip_safe=False,
-      package_dir={'SalesforcePy': 'src'},
-      install_requires=['requests', 'pytest',
-                        'responses', 'coverage', 'python-coveralls', 'pytest-flake8'],
-      tests_require=['pytest', 'responses'])
+setuptools.setup(
+    name='SalesforcePy',
+    version='1.0.1',
+    description='An absurdly simple package for making Salesforce Rest API requests',
+    url='https://github.com/forcedotcom/SalesforcePy',
+    author='Aaron Caffrey',
+    author_email='acaffrey@salesforce.com',
+    license='BSD',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+    ],
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=['requests', 'pytest',
+                    'responses', 'coverage'],
+    tests_require=['pytest', 'responses']
+)
