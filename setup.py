6c8ba576158d6e739226457acb2747501a36a805
Aaron Caffrey <39694873+caffalaughrey@users.noreply.github.com>
2018-11-06 10:02:06 +0000

    Bumped version (1.0.2)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='SalesforcePy',
-      version='1.0.1',
+      version='1.0.2',
       description='An absurdly simple package for making Salesforce Rest API requests',
       url='https://github.com/forcedotcom/SalesforcePy',
       author='Aaron Caffrey',
