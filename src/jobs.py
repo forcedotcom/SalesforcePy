267ee2afab553bc82dfa6c44f3fef8c430660921
Aaron Caffrey <acaffrey@salesforce.com>
2018-12-05 16:08:37 +0000

    Completed support for all job get requests

--- src/jobs.py
+++ src/jobs.py
@@ -12,6 +12,11 @@ import requests
 
 BATCHES_URI = '/services/data/v%s/jobs/ingest/%s/batches'
 CREATE_URI = '/services/data/v%s/jobs/ingest'
+GET_ALL_URI = '/services/data/v%s/jobs/ingest'
+GET_INFO_URI = '/services/data/v%s/jobs/ingest/%s'
+GET_SUCCESSES_URI = '/services/data/v%s/jobs/ingest/%s/successfulResults'
+GET_FAILURES_URI = '/services/data/v%s/jobs/ingest/%s/failedResults'
+GET_UNPROCESSED_URI = '/services/data/v%s/jobs/ingest/%s/unprocessedrecords'
 UPDATE_URI = '/services/data/v%s/jobs/ingest/%s'
 
 
@@ -55,6 +60,46 @@ class CreateJob(commons.BaseRequest):
         self.service = CREATE_URI % api_version
 
 
+class GetJob(commons.BaseRequest):
+    """ Performs a GET request to `'/services/data/vX.XX/jobs/ingest'`
+        .. versionadded:: 1.1.0
+    """
+    def __init__(self, session_id, instance_url, api_version, **kwargs):
+        """ Constructor. Calls `super`, then encodes the `service` including the `query_string` provided
+
+          :param: session_id: Session ID used to make request
+          :type: session_id: string
+          :param: instance_url: Instance URL used to make the request (eg. `'eu11.salesforce.com'`)
+          :type: instance_url: string
+          :param: api_version: API version
+          :type: api_version: string
+          :param: request_body: Request body
+          :type: request_body: dict
+          :param: **kwargs: kwargs
+          :type: **kwargs: dict
+        """
+        super(GetJob, self).__init__(session_id, instance_url, **kwargs)
+
+        successes = kwargs.get('successes', False)
+        failures = kwargs.get('failures', False)
+        unprocessed = kwargs.get('unprocessed', False)
+        job_id = kwargs.get('job_id', None)
+        job_info = kwargs.get('job_info', successes is False and failures is False and unprocessed is False)
+
+        self.http_method = 'GET'
+
+        if successes:
+            self.service = GET_SUCCESSES_URI % (api_version, job_id)
+        elif failures:
+            self.service = GET_FAILURES_URI % (api_version, job_id)
+        elif unprocessed:
+            self.service = GET_UNPROCESSED_URI % (api_version, job_id)
+        elif job_info and job_id is not None:
+            self.service = GET_INFO_URI % (api_version, job_id)
+        else:
+            self.service = GET_ALL_URI % api_version
+
+
 class Ingest(commons.ApiNamespace):
     def batches(self, job_id, csv_file, **kwargs):
         client = self.client
@@ -72,8 +117,13 @@ class Ingest(commons.ApiNamespace):
 
         return response, create_job
 
-    def get(self):
-        pass
+    def get(self, **kwargs):
+        client = self.client
+        api_version = self.client_kwargs.get('version')
+        get_job = GetJob(client.session_id, client.instance_url, api_version, **kwargs)
+        response = get_job.request()
+
+        return response, get_job
 
     def delete(self):
         pass
