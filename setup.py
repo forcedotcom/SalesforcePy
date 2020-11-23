de15177bfc426143e49d92433886c18d02365238
Christian Pedersen <chripede@gmail.com>
2020-11-23 14:55:51 +0100

    Make the Requests requirement dynamic
    Accept any version between 2.20.0 and 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 
-install_requires = ['requests==2.20.0', ]
+install_requires = ['requests>=2.20.0,<3', ]
 
 tests_require = [
     'responses==0.10.1',
