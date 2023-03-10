42655a1ab3ee7f161f4d8f5c9cf43a9a5d2ad897
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2023-03-10 08:45:44 -0500

    Use generator with "max()"
    instead of creating and appending to a list first.

--- SalesforcePy/sfdc.py
+++ SalesforcePy/sfdc.py
@@ -168,10 +168,7 @@ class Client(object):
             headers = {'Content-Type': 'application/json'}
             r = requests.get(service, headers=headers, proxies=self.proxies)
             if r.status_code == 200:
-                versions = []
-                for i in r.json():
-                    versions.append(i['version'])
-                self.client_kwargs.update({'version': max(versions)})
+                self.client_kwargs.update({'version': max(i['version'] for i in r.json())})
             else:
                 # return a known recent api version
                 self.client_kwargs.update({'version': DEFAULT_API_VERSION})
